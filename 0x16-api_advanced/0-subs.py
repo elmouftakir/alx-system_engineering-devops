b7d03f8d20c8e955c718bc1325677523077cce3a
elmouftakir <abderahim.mouftakir@gmail.com>
2024-03-05 10:03:12 -0800

    function that returns the total number of subscribers for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,16 +5,13 @@ Contains the number_of_subscribers function
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    if not subreddit or not isinstance(subreddit, str):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-
-    url = f'http://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
-
-    response = requests.get(url, headers=headers).json()
-    subscribers = response.get("data", {}).get("subscribers", 0)
-
-    return subscribers
-
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
