a71df51e76b6d6ccad6412a6d842fb7ceaee5733
elmouftakir <abderahim.mouftakir@gmail.com>
2024-03-05 19:24:14 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,15 +4,19 @@ this doc for module
 """
 import requests
 
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+header = {
+    "User-Agent": "MyCustomUserAgent/1.0"
+}
 
 
 def number_of_subscribers(subreddit):
     """method doc"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
+    try:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        response = requests.get(url, allow_redirects=False, headers=header)
+        if response.status_code == 200:
+            data = response.json()
+            return data["data"]["subscribers"]
+        return 0
+    except Exception:
         return 0
