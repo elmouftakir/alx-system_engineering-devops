06a835e3e61b573f4898f9bce7cd5568a07e549f
elmouftakir <abderahim.mouftakir@gmail.com>
2024-03-07 18:28:53 +0100

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,30 +2,34 @@
 """Contains recurse function"""
 import requests
 
-
-def recurse(subreddit, hot_list=[], after="", count=0):
+def recurse(subreddit, hot_list=None, after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    if hot_list is None:
+        hot_list = []
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     params = {
         "after": after,
         "count": count,
         "limit": 100
     }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
     if response.status_code == 404:
         return None
 
     results = response.json().get("data")
     after = results.get("after")
     count += results.get("dist")
+
     for c in results.get("children"):
         hot_list.append(c.get("data").get("title"))
 
     if after is not None:
         return recurse(subreddit, hot_list, after, count)
+    
     return hot_list
+
